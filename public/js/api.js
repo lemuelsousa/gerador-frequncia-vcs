c5139490546751e2edff8430619df6f869aa344f
lemuel <lemuel124.ldsp@gmail.com>
2025-08-07 11:34:27 -0300

    handle download

--- public/js/api.js
+++ public/js/api.js
@@ -1,24 +1,33 @@
 export async function submit(data) {
   try {
-    fetch("/api/names", {
+    const response = await fetch("/api/names", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-    })
-      .then((response) => response.blob())
-      .then((blob) => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = `frequencia-${data.date}.zip`;
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-        window.URL.revokeObjectURL(url);
-      });
+    });
+
+    if (!response.ok) {
+      throw new Error("Ocorreu um erro inesperado");
+    }
+
+    const blob = await response.blob();
+
+    if (blob === 0) {
+      throw new Error("Arquivo vazio recebido");
+    }
+
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `freq-${data.date}.zip`;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    window.URL.revokeObjectURL(url);
   } catch (error) {
-    console.error("There was a problem: " + error.message);
+    console.error(error.message);
+    alert("Verifique os nomes informados ou tente mais tarde.");
   }
 }
